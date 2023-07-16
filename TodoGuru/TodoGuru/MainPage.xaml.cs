1dbd5d80d72e730bf75c72ab8a81abfe1eccabb1
ModupeD <daniemu@mail.uc.edu>
2023-07-16 12:27:30 -0400

    Create Todo list

--- TodoGuru/TodoGuru/MainPage.xaml.cs
+++ TodoGuru/TodoGuru/MainPage.xaml.cs
@@ -8,12 +8,32 @@ using Xamarin.Forms;
 
 namespace TodoGuru
 {
+
+    public class TodoItem
+    {
+        public string Name { get; set; }
+    }
+
     public partial class MainPage : ContentPage
     {
+
+        private List<TodoItem> _todoItems = new List<TodoItem>();
+
         public MainPage()
         {
             InitializeComponent();
+            TodoListView.ItemsSource = _todoItems;
+        }
+        private void OnAddTodoClicked(object sender, EventArgs e)
+        {
+            var todoItem = new TodoItem { Name = TodoEntry.Text };
+            _todoItems.Add(todoItem);
+
+            TodoEntry.Text = string.Empty;
+
+            TodoListView.ItemsSource = null;
+            TodoListView.ItemsSource = _todoItems;
+
         }
     }
 }
-
