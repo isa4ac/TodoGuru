9b20f79e326e338a746d6ae1f8e3fa14a6043e54
Isaac Van Meter <ivanmeter28@gmail.com>
2023-07-26 22:10:58 -0400

    merge progress save

---
+++ TodoGuru/TodoGuru/AddTaskPage/AddTaskPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TodoGuru.AddTaskPage
+{	
+	public partial class AddTaskPage : ContentPage
+	{	
+		public AddTaskPage ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
+
