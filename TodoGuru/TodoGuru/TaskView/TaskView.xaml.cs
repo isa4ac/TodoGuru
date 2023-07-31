acfeec5f8a00f70f5eaa523d212a5f014a25cfbd
Isaac Van Meter <ivanmeter28@gmail.com>
2023-07-30 23:34:41 -0400

    Main Task Cell Look and Task View

---
+++ TodoGuru/TodoGuru/TaskView/TaskView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace TodoGuru.TaskView
+{	
+	public partial class TaskView : ContentPage
+	{
+        public TaskView (UserTask userTask)
+		{
+			InitializeComponent ();
+
+			nameLabel.Text = userTask.taskName;
+            createDateLabel.Text = userTask.logDate;
+            dueDateLabel.Text = userTask.dueDate;
+            descriptionLabel.Text = userTask.description;
+            completeLabel.Text = userTask.complete.ToString();
+            catagoryLabel.Text = userTask.Category;
+		}
+	}
+}
+
