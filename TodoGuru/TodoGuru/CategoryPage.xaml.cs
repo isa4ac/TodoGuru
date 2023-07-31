267ea66f00ad47f60bda95f8750dab27b61cf412
Guy-leroc Ossebi <thonerguyleroc1@gmail.com>
2023-07-31 18:04:54 -0400

    added picker so that user can chose which category they wants to view in the dropdown

--- TodoGuru/TodoGuru/CategoryPage.xaml.cs
+++ TodoGuru/TodoGuru/CategoryPage.xaml.cs
@@ -12,10 +12,44 @@ namespace TodoGuru
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CategoryPage : ContentPage
     {
+        private List<CategoryTask> _allCategoryTasks;
         public CategoryPage(List<CategoryTask> categoryTasks)
         {
             InitializeComponent();
             CategoryCollectionView.ItemsSource = categoryTasks;
         }
-    }
+
+        private void PopulateCategoryPicker()
+        {
+            // Get all categories from the list of CategoryTasks
+            List<string> categories = new List<string>();
+            foreach (var categoryTask in _allCategoryTasks)
+            {
+                categories.Add(categoryTask.CategoryName);
+            }
+
+            categoryPicker.ItemsSource = categories;
+        }
+
+        private void OnCategorySelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateTaskListForSelectedCategory();
+        }
+
+        private void UpdateTaskListForSelectedCategory()
+        {
+            string selectedCategory = categoryPicker.SelectedItem as string;
+
+            if (string.IsNullOrEmpty(selectedCategory))
+            {
+                CategoryCollectionView.ItemsSource = _allCategoryTasks;
+            }
+            else
+            {
+                var selectedCategoryTask = _allCategoryTasks.Find(c => c.CategoryName == selectedCategory);
+                CategoryCollectionView.ItemsSource = new List<CategoryTask> { selectedCategoryTask };
+            }
+        }
+    
+}
 }
